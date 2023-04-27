66cc3b0919c74718fc3b6f616ddcf6b71d4a3d5a
Azat <aelchikushuulu@gmail.com>
2023-04-27 21:21:33 +0600

    Initial commit

---
+++ src/main/java/com/github/aza06_06/hw_spring/HwSpringApplication.java
@@ -0,0 +1,13 @@
+package com.github.aza06_06.hw_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwSpringApplication.class, args);
+    }
+
+}
