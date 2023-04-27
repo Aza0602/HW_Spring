8ef4f75938b2fc9e035a45cb0332353b9671fe60
Azat <aelchikushuulu@gmail.com>
2023-04-27 21:39:27 +0600

    Service

---
+++ src/main/java/com/github/aza06_06/hw_spring/Service/ShoppingCartService.java
@@ -0,0 +1,28 @@
+package com.github.aza06_06.hw_spring.Service;
+
+import com.github.aza06_06.hw_spring.model.ShoppingCart;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class ShoppingCartService {
+
+    private Map<String, ShoppingCart> shoppingCarts = new ConcurrentHashMap<>();
+
+    public ShoppingCart getShoppingCart(String sessionId) {
+        return shoppingCarts.computeIfAbsent(sessionId, key -> new ShoppingCart());
+    }
+
+    public void addItem(String sessionId, int itemId) {
+        ShoppingCart shoppingCart = getShoppingCart(sessionId);
+        shoppingCart.getItemIds().add(itemId);
+    }
+
+    public List<Integer> getItems(String sessionId) {
+        ShoppingCart shoppingCart = getShoppingCart(sessionId);
+        return shoppingCart.getItemIds();
+    }
+}
